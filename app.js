671bd06b60f631829c57fa8e268bebc2d23cdc5b
basker26 <baskerron@gmail.com>
2024-05-25 21:28:48 +0530

    hi

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-// navbar collapse after  opening the menu bar
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+// Define the AngularJS application module
+var app = angular.module('myApp', ['ui.router']);
+
+// Configure the routes
+app.config(function($stateProvider, $urlRouterProvider) {
+    // For any unmatched URL, redirect to /home
+    $urlRouterProvider.otherwise('/home');
+
+    // Define the states
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: './views/home.html',
+            controller: 'HomeController'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: 'about.html',
+            controller: 'AboutController'
+        });
+});
+
+// Define the controllers
+app.controller('HomeController', function($scope) {
+    $scope.message = "Welcome to the Home Page!";
+});
+
+app.controller('AboutController', function($scope) {
+    $scope.message = "Learn more About Us.";
 });
